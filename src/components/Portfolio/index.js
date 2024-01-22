1b8b7c033292a1227fdcb1e7b4f1f3eae9d73544
Mukhode Raliphaswa <MRaliphaswa@clientele.co.za>
2024-01-22 10:54:05 +0200

    Portfolio WIP

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+
+const Portfolio = () => { 
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    });
+
+
+    return (
+        <>
+            
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+export default Portfolio;
